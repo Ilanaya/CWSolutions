34dd3aa8639a44a4eb6b4d14016d07ca5a4b6d66
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-01-28 21:59:26 +0300

    Add impertative bouncing ball solution

--- bouncingBall.js
+++ bouncingBall.js
@@ -10,3 +10,17 @@ function bouncingBall(h, bounce, window) {
 }
 console.log("first", bouncingBall(3.0, 0.66, 1.5)); // 3
 console.log("second", bouncingBall(30.0, 0.66, 1.5)); // 15
+
+function bouncingBallImp(h, bounce, window) {
+    const condition = h > 0 && bounce > 0 && bounce < 1 && window < h;
+    if (!condition) return -1;
+    let res = 1;
+    let bounceHeight = h * bounce;
+    while (bounceHeight > window) {
+        res += 2;
+        bounceHeight *= bounce;
+    }
+    return res;
+}
+console.log("first", bouncingBallImp(3.0, 0.66, 1.5)); // 3
+console.log("second", bouncingBallImp(30.0, 0.66, 1.5)); // 15
