bab913f2c220efe7c308e5de5580f1f0061b86eb
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-02-13 19:16:32 +0300

    Squash

--- BalanceParenthesis/Simple.js
+++ BalanceParenthesis/Simple.js
@@ -1,18 +1,18 @@
 //  https://www.codewars.com/kata/5d8365b570a6f6001519ecc8/train/javascript
 const fixParentheses = (str) => {
-    const stack = [];
-    const bracketsToClose = [];
+    const bracketsToCLose = [];
+    const bracketsToOpen = [];
     for (let i = 0; i <= str.length - 1; i++) {
         if (str[i] === "(") {
-            stack.push(str[i]);
+            bracketsToCLose.push(str[i]);
         } else {
-            if (stack.pop() !== "(") {
-                bracketsToClose.push(str[i]);
+            if (bracketsToCLose.pop() !== "(") {
+                bracketsToOpen.push(str[i]);
             }
         }
     }
-    const openBracksToAdd = bracketsToClose.map((_) => "(").join("");
-    const closeBracksToAdd = stack.map((_) => ")").join("");
+    const openBracksToAdd = bracketsToOpen.map((_) => "(").join("");
+    const closeBracksToAdd = bracketsToCLose.map((_) => ")").join("");
     return openBracksToAdd + str + closeBracksToAdd;
 };
 
